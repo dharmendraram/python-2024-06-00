b0d0c0d044c6aff504d85b0986077ea96ea8563e
dharmendraram <dharmendraram7852@gmail.com>
2024-07-02 07:14:30 +0545

    day 17 completed up to task of decorator

--- day17/3_task.py
+++ day17/3_task.py
@@ -4,13 +4,27 @@ of the function
 """
 import time
 
-# @time_taken
+def time_taken(func):
+    def inner_fxn(*args,**kwargs):
+        start = time.time()
+        func(*args, **kwargs)
+        end = time.time()
+        # return start
+        return end-start
+        # return end
+        
+    return inner_fxn
+
+@time_taken
 def long_loop():
     for i in range(100000000):
         pass
 
-start = time.time()
-long_loop()
-end = time.time()
+result=long_loop()
+print(f"Taken time is: {result}")
 
-print("Taken time is", end-start)
+# start = time.time()
+# long_loop()
+# end = time.time()
+
+# print("Taken time is", end-start)
